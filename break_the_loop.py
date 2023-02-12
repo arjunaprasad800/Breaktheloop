872edcb487464e51e18c315ea9840f168273f47d
arjunaprasad800 <125173478+arjunaprasad800@users.noreply.github.com>
2023-02-12 13:46:18 +0600

    Add files via upload

---
+++ break_the_loop.py
@@ -0,0 +1,40 @@
+import os
+
+def heatwave():
+    print "Is it feasible to get a real time indication of the heat wave and provide a warning?[a similar system for Tsunamis exists here 7]”. "
+    print " to ptevent any kind of natural calamity such as- 'heatwave' we must be aware of the facts of energy and time."
+    print "What is a heatwave and how we can apply digital measures to prevent it?"
+    
+    print "Heat waves are often called a silent killer and have become even more important as recent studies suggest that heat waves have become the second most devastating extreme weather event with respect to human deaths and losses."
+    print " When the wet bulb temperature exceeds the human body temperature, humans can no longer experience sensitivity"
+    def link_to_power():
+        hydropower = 1
+        air_conditioningaccess = 1
+        while hydropower = 1:
+            power_is_on= 1
+            if power_is_on= True:
+                power_is_on >air_conditioningaccess
+            
+            else:
+                exposed_to_heat=1
+        else:
+            return 0
+            
+        
+    def generizationofenergyandtime(exposedtohotclimate,power_is_on,air_conditioningaccess):
+            exposedtohotclimate=1
+            air_conditioningaccess = 1
+            if exposedtohotclimate=1:
+                print "exposedtohotclimate_percentage = 44"
+        
+            elif air_conditioningaccess = 1:
+                print "air_conditioningaccess_percntage= 35"
+            else:
+                return 0
+            
+            
+           
+        
+        
+        
+        
